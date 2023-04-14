a47d5f717a74cb71ca5d1f964225dd9812e4a67e
kooncewc <kooncewc@mail.uc.edu>
2023-04-14 17:43:51 -0400

    Add project files.

---
+++ IT3047_Final_Project01/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IT3047_Final_Project01.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
